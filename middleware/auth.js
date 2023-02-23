4f2fe527e95a0e27d5a359d5112e97a8c6325fef
Alongkot <alongkot317@gmail.com>
2023-02-24 00:29:39 +0700

    add Authorization

---
+++ middleware/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.protect=async (req,res,next)=>{
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        token = req.headers.authorization.split(" ")[1];
+    }
+    if(!token){
+        return res.status(401).json({success:false, message:'Not authorize to acess this route'});
+    }
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+        console.log(decoded);
+        req.user=await User.findById(decoded.id);
+        next();
+    } catch(err){
+        console.log(err.stack);
+        return res.status(401).json({success:false, message:'Not authorize to acess this route'});
+    }
+};
+
+exports.authorize=(...roles) =>{
+    return (req,res,next) =>{
+        if(!roles.includes(req.user.role)){
+            return res.status(403).json({succes:false,
+                message:`User role ${req.user.role} is not authorized to acess this route`});
+        }
+        next();
+    }
+}
