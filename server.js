ca4decbf0a0e4888abdc77a345e7875e9f4e0c3d
Alongkot <alongkot317@gmail.com>
2023-03-16 19:09:14 +0700

    implemant appointment functionality

--- server.js
+++ server.js
@@ -8,17 +8,15 @@ dotenv.config({path:'./config/config.env'});
 connectDB();
 
 const hospitals = require('./routes/hospitals');
+const appointments = require('./routes/appointments');
 const auth = require('./routes/auth');
-
-
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/appointments', appointments);
 app.use('/api/v1/auth',auth);
-
-
 
 const PORT = process.env.PORT || 500;
 const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, ' mode on port ', PORT));
